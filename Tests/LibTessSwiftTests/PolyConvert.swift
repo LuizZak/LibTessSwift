708ba46d460c29dcd83d25b54a4683756cf4dc0a
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2018-09-29 17:52:43 -0300

    Removing unused code

--- Tests/LibTessSwiftTests/PolyConvert.swift
+++ Tests/LibTessSwiftTests/PolyConvert.swift
@@ -1,15 +1,6 @@
-//
-//  PolyConvert.swift
-//  LibTessSwift
-//
-//  Created by Luiz Fernando Silva on 27/02/17.
-//  Copyright © 2017 CocoaPods. All rights reserved.
-//
-
-import Foundation
 import LibTessSwift
 
-class PolyConvert: NSObject {
+class PolyConvert {
     
     public static func toTess(pset: PolygonSet, tess: Tess) {
         for poly in pset.polygons {
@@ -23,29 +14,5 @@ class PolyConvert: NSObject {
             
             tess.addContour(v, poly.orientation)
         }
-    }
-
-    public static func fromTess(tess: Tess) -> PolygonSet {
-        let output = PolygonSet()
-        
-        for i in 0..<tess.elementCount {
-            let poly = Polygon()
-            
-            for j in 0..<3 {
-                let index = tess.elements[i * 3 + j]
-                if (index == -1) {
-                    continue
-                }
-                let v = PolygonPoint(
-                    x: CGFloat(tess.vertices[index].position.x),
-                    y: CGFloat(tess.vertices[index].position.y),
-                    z: 0,
-                    color: Color.white
-                )
-                poly.points.append(v)
-            }
-            output.polygons.append(poly)
-        }
-        return output
     }
 }
