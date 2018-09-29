cc74337ef880ca4d5d14e8628dabdc953f03ea5e
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2018-09-29 17:44:39 -0300

    Using MiniLexer dependency to parse test data faster

--- Package.swift
+++ Package.swift
@@ -4,8 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "LibTessSwift",
+    dependencies: [
+        Package.Dependency.package(url: "https://github.com/LuizZak/MiniLexer", from: "0.7.2")
+    ],
     targets: [
         .target(name: "LibTessSwift"),
-        .testTarget(name: "LibTessSwiftTests", dependencies: ["LibTessSwift"])
+        .testTarget(name: "LibTessSwiftTests",
+                    dependencies: ["LibTessSwift", "MiniLexer"])
     ]
 )
--- Tests/LibTessSwiftTests/DataLoader.swift
+++ Tests/LibTessSwiftTests/DataLoader.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import LibTessSwift
+import MiniLexer
 
 public struct Color {
     static var white = Color(red: 1, green: 1, blue: 1, alpha: 1)
@@ -86,13 +87,18 @@ public class DataLoader {
         var currentColor = Color.white
         var currentOrientation = ContourOrientation.original
         
-        let separation = CharacterSet.init(charactersIn: " ,\t")
-        
         while var line = reader.readLine() {
             line = line.trimmingCharacters(in: .whitespacesAndNewlines)
             
-            if(line.isEmpty) {
-                if (points.count > 0) {
+            // Comment
+            if line.hasPrefix("//") || line.hasPrefix("#") || line.hasPrefix(";") {
+                continue
+            }
+            
+            let tokenizer = TokenizerLexer<FullToken<TestDataToken>>(input: line)
+            
+            if line.isEmpty {
+                if points.count > 0 {
                     let p = Polygon(points, orientation: currentOrientation)
                     currentOrientation = ContourOrientation.original
                     polys.polygons.append(p)
@@ -100,53 +106,68 @@ public class DataLoader {
                 }
                 continue
             }
-            // Comment
-            if(line.hasPrefix("//") || line.hasPrefix("#") || line.hasPrefix(";")) {
-                continue
-            }
             
-            if(line.hasPrefix("force")) {
-                var force = line.components(separatedBy: separation).filter { !$0.isEmpty }
-                
-                if (force.count == 2) {
-                    if(force[1].localizedCaseInsensitiveCompare("cw") == .orderedSame) {
-                        currentOrientation = .clockwise
-                    }
-                    if(force[1].localizedCaseInsensitiveCompare("ccw") == .orderedSame) {
-                        currentOrientation = .counterClockwise
-                    }
+            if tokenizer.tokenType(is: .force) {
+                try tokenizer.advance(overTokenType: .force)
+                if tokenizer.tokenType(is: .cw) {
+                    try tokenizer.advance(overTokenType: .cw)
+                    currentOrientation = .clockwise
+                } else {
+                    try tokenizer.advance(overTokenType: .ccw)
+                    currentOrientation = .counterClockwise
                 }
-            } else if (line.hasPrefix("color")) {
-                var rgba = line.components(separatedBy: separation).filter { !$0.isEmpty }
+            } else if tokenizer.tokenType(is: .color) {
+                try tokenizer.advance(overTokenType: .color)
                 
-                if (rgba.count != 0) {
+                let tokens = tokenizer.allTokens().filter { $0.tokenType != .comma }
+                
+                if tokens.contains(where: { $0.tokenType != .integer }) {
+                    throw DataError.invalidInputData
+                }
+                
+                if tokens.count == 3 {
+                    let r = Int(tokens[0].value)!
+                    let g = Int(tokens[1].value)!
+                    let b = Int(tokens[2].value)!
                     
-                    // rgb
-                    if rgba.count == 4, let r = Int(rgba[1]), let g = Int(rgba[2]), let b = Int(rgba[3]) {
-                        currentColor = Color.fromRGBA(red: r, green: g, blue: b)
-                        polys.hasColors = true
-                    }
+                    currentColor = Color.fromRGBA(red: r, green: g, blue: b)
+                    polys.hasColors = true
+                } else if tokens.count == 4 {
+                    let r = Int(tokens[0].value)!
+                    let g = Int(tokens[1].value)!
+                    let b = Int(tokens[2].value)!
+                    let a = Int(tokens[3].value)!
                     
-                    // rgba
-                    if rgba.count == 5, let r = Int(rgba[1]), let g = Int(rgba[2]), let b = Int(rgba[3]), let a = Int(rgba[4]) {
-                        currentColor = Color.fromRGBA(red: r, green: g, blue: b, alpha: a)
-                        polys.hasColors = true
-                    }
+                    currentColor = Color.fromRGBA(red: r, green: g, blue: b, alpha: a)
+                    polys.hasColors = true
+                } else {
+                    throw DataError.invalidInputData
                 }
             } else {
                 var x: CGFloat = 0, y: CGFloat = 0, z: CGFloat = 0
                 
-                var xyz = line.components(separatedBy: separation).filter { !$0.isEmpty }
+                var xyz = try tokenizer.allTokens()
+                    .filter { $0.tokenType != .comma }
+                    .map { token -> CGFloat in
+                        if let double = Double(token.value.trimmingCharacters(in: .whitespacesAndNewlines)) {
+                            return CGFloat(double)
+                        }
+                        
+                        throw LexerError.syntaxError(
+                            token.range?.lowerBound ?? tokenizer.lexer.inputIndex,
+                            "Expected floating-point value, received \(token.value)"
+                        )
+                    }
                 
                 if (xyz.count != 0) {
-                    if xyz.count > 0, let value = Double(xyz[0]) {
-                        x = CGFloat(value)
+                    if xyz.count > 0 {
+                        x = CGFloat(xyz[0])
                     }
-                    if xyz.count > 1, let value = Double(xyz[1]) {
-                        y = CGFloat(value)
+                    if xyz.count > 1 {
+                        y = CGFloat(xyz[1])
                     }
-                    if xyz.count > 2, let value = Double(xyz[2]) {
-                        z = CGFloat(value)
+                    if xyz.count > 2 {
+                        z = CGFloat(xyz[2])
                     }
                     
                     points.append(PolygonPoint(x: x, y: y, z: z, color: currentColor))
@@ -211,5 +232,109 @@ public class DataLoader {
     
     enum DataError: Error {
         case invalidInputData
+    }
+}
+
+private enum TestDataToken: TokenProtocol {
+    /// Grammar for floating point numbers.
+    ///
+    /// Format grammar representation:
+    ///
+    /// ```
+    /// float-grammar:
+    ///     '-'? [0-9]+ ('.' [0-9]+)? (('E' | 'e') '-'? [0-9]+)?
+    /// ```
+    fileprivate static let floatGrammar: GrammarRule =
+            ["-"]
+            + GrammarRule.digit+
+            + ["." + GrammarRule.digit+]
+            + [("E" | "e") + ["-"] + GrammarRule.digit+]
+    
+    case eof
+    case comma
+    case force
+    case color
+    case cw
+    case ccw
+    case integer
+    case float
+    
+    static var eofToken: TestDataToken = .eof
+    
+    var tokenString: String {
+        switch self {
+        case .eof:
+            return ""
+        case .comma:
+            return ","
+        case .force:
+            return "force"
+        case .color:
+            return "color"
+        case .cw:
+            return "cw"
+        case .ccw:
+            return "ccw"
+        case .integer:
+            return "<integer>"
+        case .float:
+            return "<float>"
+        }
+    }
+    
+    func length(in lexer: Lexer) -> Int {
+        switch self {
+        case .eof:
+            return 0
+        case .comma:
+            return 1
+        case .cw:
+            return 2
+        case .ccw:
+            return 3
+        case .force, .color:
+            return 5
+        case .integer:
+            return (GrammarRule.digit+).maximumLength(in: lexer) ?? 0
+        case .float:
+            return TestDataToken.floatGrammar.maximumLength(in: lexer) ?? 0
+        }
+    }
+    
+    static func tokenType(at lexer: Lexer) -> TestDataToken? {
+        
+        if lexer.checkNext(matches: ",") {
+            return .comma
+        }
+        
+        if lexer.checkNext(matches: "force") {
+            return .force
+        }
+        if lexer.checkNext(matches: "color") {
+            return .color
+        }
+        if lexer.checkNext(matches: "cw") {
+            return .cw
+        }
+        if lexer.checkNext(matches: "ccw") {
+            return .ccw
+        }
+        
+        if lexer.safeNextCharPasses(with: Lexer.isDigit) {
+            let backtracker = lexer.backtracker()
+            lexer.advance(while: Lexer.isDigit)
+            
+            if !lexer.safeIsNextChar(equalTo: ".") {
+                return .integer
+            }
+            
+            backtracker.backtrack()
+        }
+        
+        if TestDataToken.floatGrammar.passes(in: lexer) {
+            return .float
+        }
+        
+        return .eof
     }
 }
